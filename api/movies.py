82c75adf5a7772bb7f6a069ef76211faf59f24c6
Chris Rarig <cwrarig20@gmail.com>
2022-02-02 21:22:42 -0600

    fix

--- api/movies.py
+++ api/movies.py
@@ -1,4 +1,3 @@
-from email import message
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
@@ -7,5 +6,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
+
         message = 'this works'
         self.wfile.write(message.encode())
